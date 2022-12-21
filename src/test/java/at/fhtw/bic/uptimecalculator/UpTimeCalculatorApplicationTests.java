8d1e739e690adc7f5d17a9f4cd6ff7ee84e612b2
Caner <ic20b038technikum-wien.at>
2022-12-21 15:50:50 +0100

    Initial commit

---
+++ src/test/java/at/fhtw/bic/uptimecalculator/UpTimeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.uptimecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpTimeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
